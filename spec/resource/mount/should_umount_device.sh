75f7d3550d4961bed6e4fd110a94b8ee3205d378
Cody Herriges <c.a.herriges@gmail.com>
2010-08-11 03:02:59 -0700

    Both working...found documentation errors while writing, need to investigate and fix.

--- spec/resource/mount/should_umount_device.sh
+++ spec/resource/mount/should_umount_device.sh
@@ -13,24 +13,25 @@ driver_standalone
 # Creating a filesystem in a flat file for mounting.
 mkdir -p /tmp/puppet-$$-standalone/mnt/foo_fs
 
-dd if=/dev/zero of=/tmp/puppet-$$-standalone/foo_fs bs=32MB count=1
+dd if=/dev/zero of=/tmp/puppet-$$-standalone/foo_fs bs=64MB count=1
 
 mkfs.ext2 -F /tmp/puppet-$$-standalone/foo_fs
 
-# Having puppet mount it.
+if ! mount -o loop -t ext2 /tmp/puppet-$$-standalone/foo_fs /tmp/puppet-$$-standalone/mnt/foo_fs
+  then
+    ${EXIT_NOT_APPLICABLE}
+fi
+
+# Having puppet umount it.
 MANIFEST="mount { \"/tmp/puppet-$$-standalone/mnt/foo_fs\":
-  remounts => true,
-  options => \"loop\",
-  fstype => \"ext2\",
-  ensure => mounted,
-  device => \"/tmp/puppet-$$-standalone/foo_fs\"
+  ensure => unmounted,
 }"
 
 execute_manifest<<MANIFEST_EOF
 ${MANIFEST}
 MANIFEST_EOF
 
-if mount | grep /tmp/puppet-$$-standalone/foo_fs
+if ! mount | grep /tmp/puppet-$$-standalone/mnt/foo_fs
   then
     exit ${EXIT_OK}
   else
