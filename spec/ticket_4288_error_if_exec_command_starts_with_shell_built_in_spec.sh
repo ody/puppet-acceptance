f3104d9daa37d9bd9b7018f4e0bea81294c113d9
Cody Herriges <c.a.herriges@gmail.com>
2010-08-11 00:02:02 -0700

    Test for using shell built-ins in an exec resource. Ticket #4288

---
+++ spec/ticket_4288_error_if_exec_command_starts_with_shell_built_in_spec.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Author: Cody Herriges
+
+set -e
+
+source lib/setup.sh
+driver_standalone
+
+MANIFEST="exec { 'foo':
+  logoutput => true,
+  command => 'if [ \"abc\" != \"def\" ]; then touch /tmp/puppet-$$-standalone/foo; fi',
+  path => '/bin:/sbin:/usr/bin:/usr/sbin'
+  }"
+
+execute_manifest<<MANIFEST_EOF
+${MANIFEST}
+MANIFEST_EOF
+
+if [ -f /tmp/puppet-$$-standalone/foo ]
+  then
+    exit ${EXIT_OK}
+  else
+    exit ${EXIT_FAILURE}
+fi
