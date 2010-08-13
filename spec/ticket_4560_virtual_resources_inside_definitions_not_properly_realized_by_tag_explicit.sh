7fcf9dee159f3e76407b44598409b65a4f4d514e
Cody Herriges <c.a.herriges@gmail.com>
2010-08-12 19:36:51 -0700

    Three tests to verify the documentation of the implicit tagging of resources based on parent collection.

---
+++ spec/ticket_4560_virtual_resources_inside_definitions_not_properly_realized_by_tag_explicit.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Author: Cody Herriges
+
+# Checks on the documented behaviour of all resouces within a colleciton
+# are supposed to be implicitly tagged with their collection's title.
+
+set -u
+source lib/setup.sh
+driver_master_and_agent_locally
+
+PORT=18140
+
+add_cleanup '{ test -n "${master_pid:-}" && kill "${master_pid}" ; }'
+
+# Setting up production and dev environments
+mkdir -p /tmp/puppet-$$-master/manifests \
+  /tmp/puppet-$$-master/modules \
+
+# Need a puppet.conf in place for modulepath and manifest
+PCONF="[master]
+  modulepath=/tmp/puppet-$$-master/modules
+  manifest=/tmp/puppet-$$-master/manifests/site.pp"
+
+puppet_conf<<PCONF
+${PCONF}
+PCONF
+
+SITE="node default {
+  define mydef { @file { \"/tmp/puppet-$$-agent/foo-\$name\": ensure => directory, tag => mydef } }
+    mydef { 'foo': }
+    File<| tag == 'mydef' |>
+}"
+
+cat <<SITE_EOF > "/tmp/puppet-$$-master/manifests/site.pp"
+${SITE}
+SITE_EOF
+
+# Starting the puppet master
+puppet master \
+  --vardir /tmp/puppet-$$-master-var \
+  --confdir /tmp/puppet-$$-master \
+  --rundir /tmp/puppet-$$-master \
+  --no-daemonize --autosign=true \
+  --verbose --debug --color false \
+  --certname=localhost --masterport ${PORT:-18140} &
+master_pid=$!
+
+# Wait on master port to be available.
+wait_until_master_is_listening $master_pid
+
+puppet agent -t \
+  --server localhost --masterport ${PORT} \
+  --vardir /tmp/puppet-$$-agent-var \
+  --confdir /tmp/puppet-$$-agent \
+  --rundir /tmp/puppet-$$-agent
+
+if [ -d /tmp/puppet-$$-agent/foo-foo ]
+  then
+    exit $EXIT_OK
+  else
+    exit $EXIT_FAILURE
+fi
